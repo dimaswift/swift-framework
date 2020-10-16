f7e02f3838fb89ef283f1dfe199a34a92c7f0fc2
dimaswift <dimaswift@gmail.com>
2020-10-17 02:51:06 +0300

    added ClickReminder component

---
+++ Runtime/Utils/UI/ClickReminder.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SwiftFramework.Utils.UI
+{
+    public class ClickReminder : MonoBehaviour
+    {
+        [SerializeField] private Button button = null;
+        [SerializeField] private int durationHours = 24;
+        [SerializeField] private GameObject reminderIcon = null;
+        
+        private void Start()
+        {
+            button.onClick.AddListener(OnClick);
+            if (DateTime.TryParse(PlayerPrefs.GetString(name + "_reminder", ""), out DateTime remindTime))
+            {
+                bool expired = DateTime.UtcNow - remindTime > TimeSpan.FromHours(durationHours);
+                reminderIcon.SetActive(expired);
+            }
+            else
+            {
+                reminderIcon.SetActive(true);
+            }
+        }
+
+        private void OnClick()
+        {
+            reminderIcon.SetActive(false);
+            PlayerPrefs.SetString(name + "_reminder", DateTime.UtcNow.ToString());
+            PlayerPrefs.Save();
+        }
+    }
+}
